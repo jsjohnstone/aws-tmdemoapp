7780547dbfdaac841bc989fb7b98c52f8db56014
Jamie Johnstone <jamie@tixy.co.nz>
2020-04-25 17:39:25 +0100

    Update app.py

--- tmdemoapp/app.py
+++ tmdemoapp/app.py
@@ -8,17 +8,12 @@ import requests
 import json
 import os
 
-TM_API_KEY = os.environ.get("TM_API_KEY")
-
-if not TM_API_KEY:
-    raise ValueError("No TM API KEY set for application")
-
 app = Flask(__name__)             
 
 @app.route("/")                  
 def get_events():                 
     r = requests.get(
-      'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=NZ&apikey=' + TM_API_KEY)
+      'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=NZ&apikey=123)
     data  = json.loads(r.text)
 
     return render_template('shows.html', title='Upcoming Shows', events=data['_embedded']['events']) 
