c94631e4d18b3b578f05eee83ae4c4efaa2d9c2d
Jamie Johnstone <jamie@tixy.co.nz>
2020-04-26 01:28:30 +0100

    Test blue deployment

--- tmdemoapp/app.py
+++ tmdemoapp/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
                
 @app.route("/")
     def hello():
-        return "Hello Green!"
+        return "Hello Blue!"
 
 if __name__ == "__main__":     
     app.run(host='0.0.0.0')               
