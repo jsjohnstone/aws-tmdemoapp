1e994c04d04dc947f39aa1a2c791d6a59f6ef866
Jamie Johnstone <jamie@tixy.co.nz>
2020-04-26 04:23:57 +0100

    Add tmapp

--- tmdemoapp/app.py
+++ tmdemoapp/app.py
@@ -11,10 +11,17 @@ import os
 TM_API_KEY = os.environ.get("TM_API_KEY")
 
 app = Flask(__name__)             
-               
-@app.route('/')
-def helloIndex():
-    return TM_API_KEY
+
+@app.route("/")                  
+def get_events():
+    if not TM_API_KEY:
+     return "No TM API KEY set for application"
+
+    r = requests.get(
+      'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=NZ&apikey=' + TM_API_KEY)
+    data  = json.loads(r.text)
+
+    return render_template('shows.html', title='Upcoming Shows', events=data['_embedded']['events']) 
 
 if __name__ == "__main__":     
     app.run(host='0.0.0.0')               
